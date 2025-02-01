0c519a749e21c58e3d25e2c488e7d22c5b6c34b2
Shi Johnson-Bey <shijbey@gmail.com>
2025-01-31 19:51:10 -0800

    Project setup and documentation.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [0.1.0] Unreleased
+
+_initial release._
+
+[0.1.0]: https://github.com/ShiJbey/drolta_py/releases/v0.1.0
---
+++ src/drolta/__init__.py
@@ -0,0 +1,14 @@
+"""Drolta SQLite Query Engine Wrapper.
+
+Drolta is a query engine that wraps a  SQLite database to allow
+users to write queries that are more declarative, composable,
+and legible than raw SQL. Drolta was originally developed for a
+video game-inspired simulation that relied heavily on SQLite to
+manage character and world data. We needed more accessible query
+language for users not familiar with SQL. Drolta pulls inspiration
+from logic languages like Prolog and Datalog to give users an
+easy to use database querying experience.
+
+"""
+
+__version__ = "0.1.0"
